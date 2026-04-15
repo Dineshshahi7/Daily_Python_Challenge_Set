95acb55d6b96e49218d0af5cae557adbbfe1c081
Dinesh Shahi <dineshshahi4301@gmail.com>
2026-04-15 07:08:10 +0545

    upload Day 19 folder to github

---
+++ Day_0019/inversion_count.py
@@ -0,0 +1,27 @@
+'''
+Given an array of integers arr[]. You have to find the Inversion Count of the array. 
+Note : Inversion count is the number of pairs of elements (i, j) such that i < j and arr[i] > arr[j].
+
+Examples:
+
+Input: arr[] = [2, 4, 1, 3, 5]
+Output: 3
+Explanation: The sequence 2, 4, 1, 3, 5 has three inversions (2, 1), (4, 1), (4, 3).
+Input: arr[] = [2, 3, 4, 5, 6]
+Output: 0
+Explanation: As the sequence is already sorted so there is no inversion count.
+'''
+
+def inversioncount(arr):
+    count = 0
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if i < j and arr[i] > arr[j]:
+                count += 1
+                
+    return count
+    
+    
+    
+    
+print(inversioncount([2, 4, 1, 3, 5]))
---
+++ Day_0019/parenthesis_checker.py
@@ -0,0 +1,43 @@
+'''
+Given a string s, composed of different combinations of '(' , ')', '{', '}', '[', ']'. Determine whether the Expression is balanced or not.
+An expression is balanced if:
+
+Each opening bracket has a corresponding closing bracket of the same type.
+Opening brackets must be closed in the correct order.
+Examples :
+
+Input: s = "[{()}]"
+Output: true
+Explanation: All the brackets are well-formed.
+Input: s = "[()()]{}"
+Output: true
+Explanation: All the brackets are well-formed.
+Input: s = "([]"
+Output: false
+Explanation: The expression is not balanced as there is a missing ')' at the end.
+Input: s = "([{]})"
+Output: false
+Explanation: The expression is not balanced as there is a closing ']' before the closing '}'.
+'''
+
+
+def isBalanced(s):
+    stack = []
+    pairs = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+
+    for ch in s:
+        if ch in '([{':
+            stack.append(ch)
+        else:
+            if not stack or stack[-1] != pairs[ch]:
+                return False
+            stack.pop()
+
+    return len(stack) == 0
+
+
+print(isBalanced("[{()}]"))
