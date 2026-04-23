79c0ed5da34d797a73eb9ea85f5242196e97bab1
Dinesh Shahi <dineshshahi4301@gmail.com>
2026-04-23 06:41:32 +0545

    upload Day 26 folder to github

---
+++ Day_0026/floor_in_sorted_array.py
@@ -0,0 +1,41 @@
+'''
+Given a sorted array arr[] and an integer x, find the index (0-based) of the largest element in arr[] that is less than or equal to x. This element is called the floor of x. If such an element does not exist, return -1.
+
+Note: In case of multiple occurrences of floor of x, return the index of the last occurrence.
+
+Examples
+
+Input: arr[] = [1, 2, 8, 10, 10, 12, 19], x = 5
+Output: 1
+Explanation: Largest number less than or equal to 5 is 2, whose index is 1.
+
+Input: arr[] = [1, 2, 8, 10, 10, 12, 19], x = 11
+Output: 4
+Explanation: Largest Number less than or equal to 11 is 10, whose indices are 3 and 4. The index of last occurrence is 4.
+
+Input: arr[] = [1, 2, 8, 10, 10, 12, 19], x = 0
+Output: -1
+Explanation: No element less than or equal to 0 is found. So, output is -1.
+'''
+
+def find_floor(arr, x):
+    left, right = 0, len(arr) - 1
+    ans = -1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] <= x:
+            ans = mid       
+            left = mid + 1   
+        else:
+            right = mid - 1 
+
+    return ans
+
+
+arr = [1, 2, 8, 10, 10, 12, 19]
+
+print(find_floor(arr, 5))   
+print(find_floor(arr, 11))  
+print(find_floor(arr, 0)) 
---
+++ Day_0026/remove_loop_in_lnked_list.py
@@ -0,0 +1,59 @@
+'''
+Given the head of a singly linked list, the task is to remove a cycle if present. A cycle exists when a node's next pointer points back to a previous node, forming a loop. Internally, a variable pos denotes the index of the node where the cycle starts, but it is not passed as a parameter. The terminal will print true if a cycle is removed otherwise, it will print false.
+
+Examples:
+
+Input: head = 1 -> 3 -> 4, pos = 2
+Output: true
+Explanation: The linked list looks like
+
+A loop is present in the list, and it is removed.
+Input: head = 1 -> 8 -> 3 -> 4, pos = 0
+Output: true
+Explanation: 
+
+The Linked list does not contains any loop. 
+Input: head = 1 -> 2 -> 3 -> 4, pos = 1
+Output: true
+Explanation: The linked list looks like 
+
+A loop is present in the list, and it is removed.
+Constraints:
+1 ≤ size of linked list ≤ 105
+
+
+'''
+
+class Solution:
+    def removeLoop(self, head):
+        if not head or not head.next:
+            return False
+
+        slow = head
+        fast = head
+
+        # Step 1: Detect cycle
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                break
+        else:
+            
+            return False
+
+        # Step 2: Find start of loop
+        slow = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+
+        # Step 3: Remove loop
+        ptr = slow
+        while ptr.next != slow:
+            ptr = ptr.next
+
+        ptr.next = None
+
+        return True
